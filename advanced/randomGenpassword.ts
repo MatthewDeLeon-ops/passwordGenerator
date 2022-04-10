5af2aac7192aaec697ec5bf51dff84c3ed5875e8
MatthewDeLeon-ops <68027839+MatthewDeLeon-ops@users.noreply.github.com>
2022-04-09 23:45:08 -0600

    Update randomGenpassword.ts

--- advanced/randomGenpassword.ts
+++ advanced/randomGenpassword.ts
@@ -3,9 +3,6 @@ import createPassword from "./passwordParams";
 
 program
   .option("-l, --length <number>", "length of password", "25")
-  .option("-s, --save", "save password to passwords.txt")
-  .option("-nn, --no-numbers", "remove numbers")
-  .option("-ns, --no-symbols", "remove symbols")
   .parse();
 
 const { length, numbers, symbols } = program.opts();

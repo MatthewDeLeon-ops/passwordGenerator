af3945b0876a9d843d52defbe4af64910da1d1bd
Matthew De Leon <mattjdeleon@gmail.com>
2022-04-09 21:22:20 -0600

    password generator js version added

--- passwordGenerator.ts
+++ passwordGenerator.ts
@@ -1,25 +1,13 @@
-// import parameters from "./types";
 function makePassword(maxLengthPass: any) {
   var generatedPassword = "";
-  let charactersAllowed: string =
+  const charactersAllowed: string =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  // var collectionOfCharacters: string[] = ["#", "!", "$", "%", "+", "-"];
-  var size = charactersAllowed.length;
+  const size = charactersAllowed.length;
   for (var i = 0; i < maxLengthPass; ++i) {
     generatedPassword =
       generatedPassword +
-      charactersAllowed.charAt(Math.floor(Math.random() * size))
+      charactersAllowed.charAt(Math.ceil(Math.random() * size));
   }
   return generatedPassword;
 }
-console.log(makePassword(20));
-
-// Math.floor(Math.random() * (max - min) + min)
-// collectionOfCharacters.charAt(Math.floor(Math.random() * (max - min) + min));
-
-//   collectionOfCharacters.charAt(Math.floor(Math.random() * charsize));
-// collectionOfCharacters.charCodeAt(Math.floor * (max - min));
-// Math.
-// var collectionOfCharacters = ["#", "!", "$", "%", "+", "-"];
-// var charsize = collectionOfCharacters;
-// console.log(charsize.charAt(2))
+console.log(makePassword(24));
